56d08c3f00b2aace9cfde9606c4a79c9416898a4
Kevin Vu <kevin.vu@crometrics.com>
2023-06-26 09:50:06 -0500

    redeploy

--- src/lib/_includes/supabaseClient.ts
+++ src/lib/_includes/supabaseClient.ts
@@ -4,4 +4,5 @@ import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/publi
 export const SUPABASE_URL = process.env.NODE_ENV === 'production' ? process.env.SUPABASE_URL as string : PUBLIC_SUPABASE_URL
 export const SUPABASE_ANON_KEY = process.env.NODE_ENV === 'production' ? process.env.SUPABASE_ANON_KEY as string : PUBLIC_SUPABASE_ANON_KEY
 
+
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
