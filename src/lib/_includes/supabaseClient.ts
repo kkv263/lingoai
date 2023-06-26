fb336f55e6c2fae0f2a7c58f5fd8401d5b96b2b7
Kevin Vu <kevin.vu@crometrics.com>
2023-06-26 10:00:31 -0500

    change names to public

--- src/lib/_includes/supabaseClient.ts
+++ src/lib/_includes/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
-const SUPABASE_URL = process.env.NODE_ENV === 'production' ? process.env.SUPABASE_URL as string : PUBLIC_SUPABASE_URL
-const SUPABASE_ANON_KEY = process.env.NODE_ENV === 'production' ? process.env.SUPABASE_ANON_KEY as string : PUBLIC_SUPABASE_ANON_KEY
+const SUPABASE_URL = process.env.NODE_ENV === 'production' ? process.env.PUBLIC_SUPABASE_URL as string : PUBLIC_SUPABASE_URL
+const SUPABASE_ANON_KEY = process.env.NODE_ENV === 'production' ? process.env.PUBLIC_SUPABASE_ANON_KEY as string : PUBLIC_SUPABASE_ANON_KEY
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
